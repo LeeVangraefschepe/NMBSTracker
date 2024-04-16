81e332a8c656a50754b68bdcdd3f18dde385b3ef
LEEVGS <lee.vgs@hotmail.com>
2024-04-16 22:16:43 +0200

    Update version number

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NMBS Tracker")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Track your train department time")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyCompany("LEEVGS")]
 [assembly: AssemblyProduct("NMBSTracker")]
 [assembly: AssemblyCopyright("Copyright © LEEVGS 2024")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.1.1.0")]
+[assembly: AssemblyFileVersion("1.1.1.0")]
